74da759b878896ef21473e753ced938d5763ccf1
Bindu <97860002+BinduGitRepo@users.noreply.github.com>
2025-10-11 21:34:57 -0400

    Add files via upload
    day1

---
+++ day1/Firefox.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Firefox {
+	
+	public static void main(String[] args) {
+		
+		double browserVersion = 100.2;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo ='f';
+		
+		System.out.println("browserVersion = " + browserVersion + "\n" +
+						   "browserName = " + browserName +  "\n" + 
+						   "isVisible = " +isVisible +  "\n" +
+						   "releaseYear = " + releaseYear +  "\n" +
+						   "browserLogo = " + browserLogo);
+	}
+
+}
